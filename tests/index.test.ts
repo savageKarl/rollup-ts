b0177be831c8dbd509b2c759a771fd4eb78dfdc4
savage <2443005004@qq.com>
2023-08-31 14:58:03 +0800

    test: change default test file content

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,5 @@
 import { test, expect } from '@jest/globals'
 
-import { sum, say } from 'rollup-ts'
-
-test('sum', () => {
-	expect(sum(1, 2)).toBe(3)
-})
-
-test('say', () => {
-	expect(say('hello')).toEqual('hello')
+test('hello', () => {
+	expect(1).toBe(1)
 })
