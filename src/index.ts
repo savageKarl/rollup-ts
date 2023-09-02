0adec185cce3c704b32b829351796c4c218653d1
savage <2443005004@qq.com>
2023-09-02 21:07:35 +0800

    refactor: use pnpm

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ export function sum(x: number, y: number) {
 	return x + y
 }
 
-const a = new Set()
-console.log('a: ', a)
-
 export function say(msg: string) {
 	return msg
 }
+
+console.log('hello, worldssssssssssssssssssssssssssssssssss')
