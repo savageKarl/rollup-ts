56447214c94cb2ef65a9b31b209b05a0158f841f
savage <2443005004@qq.com>
2023-09-02 21:21:42 +0800

    feat: use typedoc and vitepress

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
+/**
+ * @packageDocumentation
+ *	rollup-ts is boilerplate, used to reuse code .
+ */
+
+/**
+ * what do you  want
+ *
+ * @param x - fisrt number
+ * @param y -  secode number
+ * @returns
+ */
 export function sum(x: number, y: number) {
 	return x + y
 }
 
+/**
+ * hi
+ * @param msg -  i don't konw
+ * @returns whtat you say?
+ */
 export function say(msg: string) {
 	return msg
 }
-
-console.log('hello, worldssssssssssssssssssssssssssssssssss')
